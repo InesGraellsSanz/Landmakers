006462d8437753c706a184d29aae5c31dc76773e
inesgraellssanz <inesgraells3@gmail.com>
2025-08-16 14:26:56 +0200

    Initial Commit

---
+++ Landmakers/LandmakersApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmakersApp.swift
+//  Landmakers
+//
+//  Created by Ines Graells Sanz on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
