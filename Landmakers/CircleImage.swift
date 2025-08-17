88f3ddf6fcc539e7f933a697adf6a04ee54fae73
InesGraellsSanz <inesgraells3@gmail.com>
2025-08-17 13:48:11 +0200

    Completed Landmakers app

---
+++ Landmakers/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  Landmakers
+//
+//  Created by Ines Graells Sanz on 17/8/25.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
