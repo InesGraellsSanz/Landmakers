6678a561fde89cf27ab7941adb69aa89eb5914d2
InesGraellsSanz <inesgraells3@gmail.com>
2025-08-18 13:02:31 +0200

    Lists and navigation built

---
+++ Landmakers/Model/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  Landmakers
+//
+//  Created by Ines Graells Sanz on 17/8/25.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var name: String
+    var category: String?
+    var city: String?
+    var state: String
+    var id: Int
+    var isFeatured: Bool?
+    var isFavorite: Bool?
+    var park: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmakers/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  Landmakers
+//
+//  Created by Ines Graells Sanz on 17/8/25.
+//
+
+import Foundation
