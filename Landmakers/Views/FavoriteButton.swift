2119030c9f105a9985251ced49c83dce25b2d19e
InesGraellsSanz <inesgraells3@gmail.com>
2025-08-18 19:56:46 +0200

    Handling user input

---
+++ Landmakers/Views/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  Landmakers
+//
+//  Created by Ines Graells Sanz on 18/8/25.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
