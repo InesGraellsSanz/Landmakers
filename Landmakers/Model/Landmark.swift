93e3bb006ffec2adc736ce50f2c5d85274a9d6f0
InesGraellsSanz <inesgraells3@gmail.com>
2025-08-24 17:52:43 +0200

    All changes

--- Landmakers/Model/Landmark.swift
+++ Landmakers/Model/Landmark.swift
@@ -11,14 +11,20 @@ import CoreLocation
 
 struct Landmark: Hashable, Codable, Identifiable {
     var name: String
-    var category: String?
     var city: String?
     var state: String
     var id: Int
-    var isFeatured: Bool?
+    var isFeatured: Bool
     var isFavorite: Bool
     var park: String
     var description: String
+    
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
     
     private var imageName: String
     var image: Image {
